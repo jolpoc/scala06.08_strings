94b3ee701d1a86761c91a6c70820be757919c12e
Jolanta Pockaja <jolantapockaja@gmail.com>
2020-08-06 21:22:24 +0300

    commit #3

--- src/main/scala/FoodCalculator.scala
+++ src/main/scala/FoodCalculator.scala
@@ -5,6 +5,6 @@ class FoodCalculator extends App {
 
   // println how much will it cost to buy the food
   // it will cost xxx to buy xx kilos of  xxxx
-println("It will cost xxx to buy xx kilos of  xxxx")
+println("It will cost xxx to buy xx kilos of  xxx")
 
 }
